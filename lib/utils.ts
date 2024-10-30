eb2c0231184905d53820190327c180280f25629a
Kevin Hou <strawberrypi64@gmail.com>
2024-10-30 17:44:50 +1100

    chore: add formatCamelCase function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCamelCase(string: string) {
+  return string
+    .replace(/([A-Z])/g, " $1")
+    .replace(/^./, (str) => str.toUpperCase())
+    .trim();
+}
