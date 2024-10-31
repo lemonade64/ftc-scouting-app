5c4c24ebfde875fa79f0df2a8fbf1089b387659a
Kevin Hou <strawberrypi64@gmail.com>
2024-11-01 08:42:47 +1100

    feat: add offline pwa form

---
+++ app/~offline/page.jsx
@@ -0,0 +1,11 @@
+import PWAForm from "@/components/form/PWAForm";
+import DataImporter from "@/components/form/DataImporter";
+
+export default function Scout() {
+  return (
+    <>
+      <PWAForm />
+      <DataImporter />
+    </>
+  );
+}
