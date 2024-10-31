46acaa1d1c98830edccbb942b9ff0ecb1b5c17eb
Kevin Hou <strawberrypi64@gmail.com>
2024-10-31 12:51:24 +1100

    feat: add google sheets integration

--- app/actions/submit.js
+++ app/actions/submit.js
@@ -1,21 +1,85 @@
 "use server";
 
-import { z } from "zod";
-import { formSchema } from "@/lib/schema";
+import { google } from "googleapis";
 
-export async function submitForm(formData) {
+export async function submit(formData) {
+  const spreadsheetId = formData.get("spreadsheetID");
+  if (!spreadsheetId) {
+    throw new Error("Spreadsheet ID is Required");
+  }
+
   try {
-    const validatedData = formSchema.parse(formData);
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      },
+      scopes: [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/spreadsheets",
+      ],
+    });
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const sheets = google.sheets({ version: "v4", auth });
+    //   validatedData.teamNumber,
+    //   validatedData.teamName,
+    //   validatedData.qualificationNumber,
+    //   validatedData.allianceColour,
+    //   validatedData.autoPreload,
+    //   validatedData.autoBasketHigh,
+    //   validatedData.autoBasketLow,
+    //   validatedData.autoChamberHigh,
+    //   validatedData.autoChamberLow,
+    //   validatedData.teleopBasketHigh,
+    //   validatedData.teleopBasketLow,
+    //   validatedData.teleopChamberHigh,
+    //   validatedData.teleopChamberLow,
+    //   JSON.stringify(validatedData.teleopCycleTimes),
+    //   validatedData.endgameAscentLevel,
+    //   validatedData.endgameAscentTime,
+    //   validatedData.extraNotes,
+    // ];
 
-    console.log("Form Data Submitted:", validatedData);
+    const values = [
+      formData.get("teamNumber"),
+      formData.get("teamName"),
+      formData.get("qualificationNumber"),
+      formData.get("allianceColour"),
+      formData.get("autoPreload"),
+      formData.get("autoBasketHigh"),
+      formData.get("autoBasketLow"),
+      formData.get("autoChamberHigh"),
+      formData.get("autoChamberLow"),
+      formData.get("teleopBasketHigh"),
+      formData.get("teleopBasketLow"),
+      formData.get("teleopChamberHigh"),
+      formData.get("teleopChamberLow"),
+      formData.get("teleopCycleTimes"),
+      formData.get("endgameAscentLevel"),
+      formData.get("endgameAscentTime"),
+      formData.get("extraNotes"),
+    ];
 
-    return { success: "Form Submitted Successfully" };
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: "Sheet1",
+      valueInputOption: "RAW",
+      requestBody: {
+        values: [values],
+      },
+    });
+
+    return { success: true, message: "Data Submitted Successfully" };
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      return { error: error.errors.map((e) => e.message).join(", ") };
+    console.error("Error Submitting Form:", error);
+    if (error.errors) {
+      const errorMessages = error.errors
+        .map((err) => `${err.path.join(".")}: ${err.message}`)
+        .join(", ");
+      return { success: false, message: `Validation Failed: ${errorMessages}` };
     }
-    return { error: "Unexpected Error Iccurred" };
+    return { success: false, message: "Failed To Submit Data" };
   }
 }
